723461731a6f4b286a6c78da274edb31ed578967
ccankayali <volkand135@gmail.com>
2024-02-19 11:38:51 +0300

    auth

---
+++ panavote/src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'yourSecretKey',
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ panavote/src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
