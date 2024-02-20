8852703d2d5b339be87580d40232fb9bfd19977c
ccankayali <volkand135@gmail.com>
2024-02-20 11:34:29 +0300

    app

--- panavote/src/app.module.ts
+++ panavote/src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as dotenv from 'dotenv'; // Import dotenv
+import * as dotenv from 'dotenv';
+import { UserController } from './controller/user.controller';
+import { AuthController } from './controller/auth.controller';
+import { UserService } from './service/user.service';
+import { AuthService } from './service/auth.service';
+import { UserModule } from './module/user.module';
+import { AuthModule } from './module/auth.module';
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Initialize ConfigModule
-    MongooseModule.forRoot(process.env.MONGODB_URI), // Use environment variable for MongoDB URI
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule {}
